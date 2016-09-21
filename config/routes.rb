06f77d0da718339a33c1badf6c4e61447e0c23d8
Adepoju Adebayo <mradeybee@gmail.com>
2016-09-21 11:49:16 +0100

    Revert "add angular integration"
    This reverts commit b996db57e71837bca9292fbd5df20cf277a6152b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "application#app"
+  root to: "products#index"
   resources :products, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
